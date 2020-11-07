a47aceb26d22dbc24e6a3867b11f942b59553b24
Ray OHare <raymond.ohare@protirus.com>
2020-11-07 19:54:04 +0000

    Add project files.

---
+++ Fibonacci.Tests/FibonacciTests.cs
@@ -0,0 +1,84 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Numerics;
+
+namespace Fibonacci.Tests
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        public void Should_Return_CorrectResult_When_nthTerm_Is_0()
+        {
+            const int nthTerm = 0;
+            BigInteger expectedResult = 0;
+
+            BigInteger result = Fibonacci.CalculateFibonacci(nthTerm);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "nth Term must be 0 or greater.")]
+        public void Should_Throw_Exception_When_nthTerm_Is_LessThan_Zero()
+        {
+            const int nthTerm = -1;
+            BigInteger result = Fibonacci.CalculateFibonacci(nthTerm);
+        }
+
+        [TestMethod]
+        public void Should_Return_CorrectResult_When_nthTerm_Is_1()
+        {
+            const int nthTerm = 1;
+            BigInteger expectedResult = 0;
+
+            BigInteger result = Fibonacci.CalculateFibonacci(nthTerm);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Should_Return_CorrectResult_When_nthTerm_Is_2()
+        {
+            const int nthTerm = 2;
+            BigInteger expectedResult = 1;
+
+            BigInteger result = Fibonacci.CalculateFibonacci(nthTerm);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Should_Return_CorrectResult_When_nthTerm_Is_3()
+        {
+            const int nthTerm = 3;
+            BigInteger expectedResult = 2;
+
+            BigInteger result = Fibonacci.CalculateFibonacci(nthTerm);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Should_Return_CorrectResult_When_nthTerm_Is_10()
+        {
+            const int nthTerm = 10;
+            BigInteger expectedResult = 55;
+
+            BigInteger result = Fibonacci.CalculateFibonacci(nthTerm);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Should_Return_CorrectResult_When_nthTerm_Is_100()
+        {
+            const int nthTerm = 100;
+            BigInteger expectedResult = BigInteger.Parse("354224848179261915075");
+
+            BigInteger result = Fibonacci.CalculateFibonacci(nthTerm);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)        
+        {
+            int nthTerm = 4;
+
+            BigInteger result = Fibonacci.CalculateFibonacci(nthTerm);
+            Console.WriteLine($"F{nthTerm} is {result}");
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+        }
+    }
+
+    public static class Fibonacci
+    {
+        public static BigInteger CalculateFibonacci(int nthTerm)
+        {
+            if (nthTerm < 0)
+                throw new ArgumentOutOfRangeException("nth Term must be 0 or greater.");
+
+            BigInteger num1 = 0;
+            BigInteger num2 = 1;
+            BigInteger sum = 0;
+            for (int i=2; i <= nthTerm; i++)
+            {  
+                sum = num1 + num2;
+                num1 = num2;
+                num2 = sum;
+            }
+            return sum;
+        }
+    }
+}
