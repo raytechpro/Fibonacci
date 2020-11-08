410736d8936c7b0ce1d591345967632fe8f604c1
Ray OHare <raymond.ohare@protirus.com>
2020-11-08 08:39:58 +0000

    Add separate class for Fibonacci and XML comments

---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Fibonacci
+{
+    /// <summary>
+    /// Fibonacci methods 
+    /// </summary>
+    public static class Fibonacci
+    {
+        /// <summary>
+        /// Calculates Fibonacci nth Term 
+        /// Fₙ = Fₙ₋₂ + Fₙ₋₁
+        /// </summary>
+        /// <returns>
+        /// The Fibonacci value 
+        /// </returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">
+        /// Thrown when nthTerm parameter is less than 0
+        /// </exception>
+        /// <param name="nthTerm">nth Term to find</param>
+        public static BigInteger CalculateFibonacci(int nthTerm)
+        {
+            if (nthTerm < 0)
+                throw new ArgumentOutOfRangeException("nth Term must be 0 or greater.");
+
+            BigInteger num1 = 0;
+            BigInteger num2 = 1;
+            BigInteger sum = 0;
+            for (int i = 2; i <= nthTerm; i++)
+            {
+                sum = num1 + num2;
+                num1 = num2;
+                num2 = sum;
+            }
+            return sum;
+        }
+    }
+}
--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -7,32 +7,12 @@ namespace Fibonacci
     {
         static void Main(string[] args)        
         {
-            int nthTerm = 4;
+            int nthTerm = 100;
 
             BigInteger result = Fibonacci.CalculateFibonacci(nthTerm);
             Console.WriteLine($"F{nthTerm} is {result}");
             Console.WriteLine("Press a key to exit");
             Console.ReadKey();
-        }
-    }
-
-    public static class Fibonacci
-    {
-        public static BigInteger CalculateFibonacci(int nthTerm)
-        {
-            if (nthTerm < 0)
-                throw new ArgumentOutOfRangeException("nth Term must be 0 or greater.");
-
-            BigInteger num1 = 0;
-            BigInteger num2 = 1;
-            BigInteger sum = 0;
-            for (int i=2; i <= nthTerm; i++)
-            {  
-                sum = num1 + num2;
-                num1 = num2;
-                num2 = sum;
-            }
-            return sum;
         }
     }
 }
